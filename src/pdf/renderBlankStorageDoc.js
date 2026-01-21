f7009cb6ec0ea686431e5a6fa0f2315bf67df954
Aakash Sharma <aakash.sharma@capgemini.com>
2026-01-21 06:58:40 +0000

    Resolved Sonar Duplication issue

--- src/pdf/renderBlankStorageDoc.js
+++ src/pdf/renderBlankStorageDoc.js
@@ -24,21 +24,42 @@ const DOC_NUMBER_FONT_SIZE = 10;
 const WATERMARK_X_POSITION = 30;
 const WATERMARK_Y_POSITION = 100;
 
+// Page index constants
+const FIRST_PAGE_INDEX = 0;
+const SECOND_PAGE_INDEX = 1;
+const FIRST_WATERMARK_PAGE_INDEX = 2;
+const SECOND_WATERMARK_PAGE_INDEX = 3;
+const THIRD_WATERMARK_PAGE_INDEX = 4;
+
+// Template file constants
+const STORAGE_DOC_TEMPLATE_FILE = 'storage-doc-blank.pdf';
+const SAMPLE_WATERMARK_FILE = 'sample-watermark.png';
+const QR_TEXT_FONT_FILE = 'fonts/arial.ttf';
+
 const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate) => {
-    const inStream = new muhammara.PDFRStreamForFile(pathToTemplate + 'storage-doc-blank.pdf');
+    const inStream = new muhammara.PDFRStreamForFile(pathToTemplate + STORAGE_DOC_TEMPLATE_FILE);
     const pdfStream = new PDFStreamForNodeJsStream(stream);
     let pdfWriter = muhammara.createWriterToModify(inStream, pdfStream);
     let watermarkStreamImageXObject, imageXObject;
     if (isSample) {
-        const sampleWatermarkStream = new muhammara.PDFRStreamForFile(pathToTemplate + 'sample-watermark.png'); // './src/resources/export-cert-blank.pdf'
+        const sampleWatermarkStream = new muhammara.PDFRStreamForFile(pathToTemplate + SAMPLE_WATERMARK_FILE);
         watermarkStreamImageXObject = pdfWriter.createFormXObjectFromPNG(sampleWatermarkStream);
     } else {
         const qrCodeBuffer = await PdfUtils.generateQRCode(uri);
         const PdfImgStream = new muhammara.PDFRStreamForBuffer(qrCodeBuffer);
         imageXObject = pdfWriter.createFormXObjectFromPNG(PdfImgStream);
     }
-    let pageModifier = new muhammara.PDFPageModifier(pdfWriter, 0);
-    let ctx = pageModifier.startContext().getContext();
+    renderFirstPage(pdfWriter, pathToTemplate, data, isSample, watermarkStreamImageXObject);
+    renderSecondPage(pdfWriter, pathToTemplate, isSample, watermarkStreamImageXObject, imageXObject);
+    renderAdditionalPages(pdfWriter, watermarkStreamImageXObject, isSample);
+
+    pdfWriter.end();
+    stream.end();
+};
+
+const renderFirstPage = (pdfWriter, pathToTemplate, data, isSample, watermarkStreamImageXObject) => {
+    const pageModifier = new muhammara.PDFPageModifier(pdfWriter, FIRST_PAGE_INDEX);
+    const ctx = pageModifier.startContext().getContext();
     let docNumber = data.documentNumber;
 
     if (isSample) {
@@ -52,9 +73,11 @@ const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate
     );
 
     pageModifier.endContext().writePage();
+};
 
-    pageModifier = new muhammara.PDFPageModifier(pdfWriter, 1);
-    ctx = pageModifier.startContext().getContext();
+const renderSecondPage = (pdfWriter, pathToTemplate, isSample, watermarkStreamImageXObject, imageXObject) => {
+    const pageModifier = new muhammara.PDFPageModifier(pdfWriter, SECOND_PAGE_INDEX);
+    const ctx = pageModifier.startContext().getContext();
 
     if (isSample) {
         renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
@@ -63,11 +86,6 @@ const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate
     }
 
     pageModifier.endContext().writePage();
-    
-    renderAdditionalPages(pdfWriter, watermarkStreamImageXObject, isSample);
-
-    pdfWriter.end();
-    stream.end();
 };
 
 const renderQrCode = (pathToTemplate, pdfWriter, ctx, imageXObject, x, y) => {
@@ -80,17 +98,17 @@ const renderQrCode = (pathToTemplate, pdfWriter, ctx, imageXObject, x, y) => {
     ctx.writeText(
         'Use the QR code',
         x + QR_TEXT_X_OFFSET, y + QR_TEXT_FIRST_LINE_Y_OFFSET,
-        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_FILE),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
     ctx.writeText(
         'to check that this',
         x + QR_TEXT_X_OFFSET, y + QR_TEXT_SECOND_LINE_Y_OFFSET,
-        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_FILE),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
     ctx.writeText(
         'certificate is valid',
         x + QR_TEXT_X_OFFSET, y + QR_TEXT_THIRD_LINE_Y_OFFSET,
-        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_FILE),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
 }
 
@@ -104,9 +122,9 @@ const renderSampleWatermark = (pdfWriter, ctx, imageXObject, x, y) => {
 
 const renderAdditionalPages = (pdfWriter, watermarkStreamImageXObject, isSample) => {
     if (isSample) {
-        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, 2);
-        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, 3);
-        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, 4);
+        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, FIRST_WATERMARK_PAGE_INDEX);
+        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, SECOND_WATERMARK_PAGE_INDEX);
+        renderWatermarkPage(pdfWriter, watermarkStreamImageXObject, THIRD_WATERMARK_PAGE_INDEX);
     }
 };
 
