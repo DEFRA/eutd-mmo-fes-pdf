ee28d577e78128043727fb634f95a27d0794a0cc
Aakash Sharma <aakash.sharma@capgemini.com>
2026-01-21 06:23:56 +0000

    Fix QR code Alignment

--- src/pdf/renderBlankStorageDoc.js
+++ src/pdf/renderBlankStorageDoc.js
@@ -4,6 +4,26 @@ const PdfUtils = require('./mmoPdfUtils');
 const muhammara = require('muhammara');
 const PDFStreamForNodeJsStream = require('./PDFStreamForNodeJsStream');
 
+// QR Code rendering constants
+const QR_CODE_X_POSITION = 320;
+const QR_CODE_Y_POSITION = 100;
+const QR_TEXT_X_OFFSET = 65;
+const QR_TEXT_FIRST_LINE_Y_OFFSET = 38;
+const QR_TEXT_SECOND_LINE_Y_OFFSET = 24;
+const QR_TEXT_THIRD_LINE_Y_OFFSET = 10;
+const QR_TEXT_FONT_SIZE = 11;
+const QR_TEXT_COLOR = 0x00;
+const QR_TEXT_FONT_PATH = 'fonts/arial.ttf';
+
+// Document number constants
+const DOC_NUMBER_X_POSITION = 130;
+const DOC_NUMBER_Y_POSITION = 716;
+const DOC_NUMBER_FONT_SIZE = 10;
+
+// Watermark constants
+const WATERMARK_X_POSITION = 30;
+const WATERMARK_Y_POSITION = 100;
+
 const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate) => {
     const inStream = new muhammara.PDFRStreamForFile(pathToTemplate + 'storage-doc-blank.pdf');
     const pdfStream = new PDFStreamForNodeJsStream(stream);
@@ -23,12 +43,12 @@ const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate
 
     if (isSample) {
         docNumber = '###-####-##-#########';
-        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, 30, 100);
+        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
     }
     ctx.writeText(
         docNumber,
-        130, 716,
-        {font:pdfWriter.getFontForFile(pathToTemplate + 'fonts/arial.ttf'),size:10,colorspace:'gray',color:0x00}
+        DOC_NUMBER_X_POSITION, DOC_NUMBER_Y_POSITION,
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:DOC_NUMBER_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
 
     if (isSample) {
@@ -39,25 +59,25 @@ const renderBlankStorageDoc = async (data, isSample, uri, stream, pathToTemplate
     pageModifier = new muhammara.PDFPageModifier(pdfWriter, 1);
     ctx = pageModifier.startContext().getContext();
 
-    if(isSample) renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, 30, 100);
-    else renderQrCode(pathToTemplate, pdfWriter, ctx, imageXObject, 290, 150);
+    if(isSample) renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
+    else renderQrCode(pathToTemplate, pdfWriter, ctx, imageXObject, QR_CODE_X_POSITION, QR_CODE_Y_POSITION);
 
     pageModifier.endContext().writePage();
     
     if (isSample) {
         pageModifier = new muhammara.PDFPageModifier(pdfWriter, 2);
         ctx = pageModifier.startContext().getContext();
-        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, 30, 100);
+        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
         pageModifier.endContext().writePage();
 
         pageModifier = new muhammara.PDFPageModifier(pdfWriter, 3);
         ctx = pageModifier.startContext().getContext();
-        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, 30, 100);
+        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
         pageModifier.endContext().writePage();
 
         pageModifier = new muhammara.PDFPageModifier(pdfWriter, 4);
         ctx = pageModifier.startContext().getContext();
-        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, 30, 100);
+        renderSampleWatermark(pdfWriter, ctx, watermarkStreamImageXObject, WATERMARK_X_POSITION, WATERMARK_Y_POSITION);
         pageModifier.endContext().writePage();
     }
 
@@ -74,18 +94,18 @@ const renderQrCode = (pathToTemplate, pdfWriter, ctx, imageXObject, x, y) => {
 
     ctx.writeText(
         'Use the QR code',
-        x + 95, y + 50,
-        {font:pdfWriter.getFontForFile(pathToTemplate + 'fonts/arial.ttf'),size:11,colorspace:'gray',color:0x00}
+        x + QR_TEXT_X_OFFSET, y + QR_TEXT_FIRST_LINE_Y_OFFSET,
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
     ctx.writeText(
         'to check that this',
-        x + 95, y + 36,
-        {font:pdfWriter.getFontForFile(pathToTemplate + 'fonts/arial.ttf'),size:11,colorspace:'gray',color:0x00}
+        x + QR_TEXT_X_OFFSET, y + QR_TEXT_SECOND_LINE_Y_OFFSET,
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
     ctx.writeText(
         'certificate is valid',
-        x + 95, y + 22,
-        {font:pdfWriter.getFontForFile(pathToTemplate + 'fonts/arial.ttf'),size:11,colorspace:'gray',color:0x00}
+        x + QR_TEXT_X_OFFSET, y + QR_TEXT_THIRD_LINE_Y_OFFSET,
+        {font:pdfWriter.getFontForFile(pathToTemplate + QR_TEXT_FONT_PATH),size:QR_TEXT_FONT_SIZE,colorspace:'gray',color:QR_TEXT_COLOR}
     );
 }
 
