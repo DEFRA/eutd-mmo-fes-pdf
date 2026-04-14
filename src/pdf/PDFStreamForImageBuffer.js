5257fcf47c095bf8870a4e17a1beaae0350ea03f
Sal <sal.abuewilly@capgemini.com>
2026-04-14 23:10:44 +0100

    refactor: convert to ES6 class syntax
    - PDFStreamForImageBuffer: prototype to ES6 class
    - MultiDictHelper: prototype to ES6 class
    - Remove redundant  git status operator
    - Add eslint-disable for PascalCase class names

--- src/pdf/PDFStreamForImageBuffer.js
+++ src/pdf/PDFStreamForImageBuffer.js
@@ -2,39 +2,42 @@
     PDFStreamForImageBuffer is an implementation of a write stream that writes directly to a NodeJs stream.
     Using this stream frees the user from having to create a PDF file on disk when generating on-demand PDFs
 */
-function PDFStreamForImageBuffer(buffer) {
-    this.arr = Object.values(buffer);
-    this.position = 0;
-}
-
-PDFStreamForImageBuffer.prototype.read = function(inAmount) {
-    let endPos = this.position + inAmount;
-    if (endPos > this.arr.length - 1) {
-        endPos = this.arr.length - 1;
+// eslint-disable-next-line no-use-before-define
+class PDFStreamForImageBuffer {
+    constructor(buffer) {
+        this.arr = Object.values(buffer);
+        this.position = 0;
     }
-    const result = this.arr.slice(this.position, endPos);
-    this.position = endPos + 1;
-    return result;
-};
 
-PDFStreamForImageBuffer.prototype.notEnded = function() {
-    return this.position < this.arr.length;
-};
+    read(inAmount) {
+        let endPos = this.position + inAmount;
+        if (endPos > this.arr.length - 1) {
+            endPos = this.arr.length - 1;
+        }
+        const result = this.arr.slice(this.position, endPos);
+        this.position = endPos + 1;
+        return result;
+    }
 
-PDFStreamForImageBuffer.prototype.setPosition = function(pos) {
-    this.position = pos;
-};
+    notEnded() {
+        return this.position < this.arr.length;
+    }
 
-PDFStreamForImageBuffer.prototype.setPositionFromEnd = function(pos) {
-    this.position = this.arr.length - 1 - pos;
-};
+    setPosition(pos) {
+        this.position = pos;
+    }
 
-PDFStreamForImageBuffer.prototype.skip = function(inAmount) {
-    this.position += inAmount;
-};
+    setPositionFromEnd(pos) {
+        this.position = this.arr.length - 1 - pos;
+    }
 
-PDFStreamForImageBuffer.prototype.getCurrentPosition = function() {
-    return this.position;
-};
+    skip(inAmount) {
+        this.position += inAmount;
+    }
+
+    getCurrentPosition() {
+        return this.position;
+    }
+}
 
 module.exports = PDFStreamForImageBuffer;
--- src/utils/pdf-text-extraction/multi-dict-helper.js
+++ src/utils/pdf-text-extraction/multi-dict-helper.js
@@ -1,25 +1,28 @@
 const _ = require('lodash');
 
-function MultiDictHelper(dicts) {
-    this.dicts = dicts;
-}
-
-MultiDictHelper.prototype.exists = function(name) {
-    return _.some(this.dicts,(dict)=> {
-        return dict.exists(name);
-    });
-}
-
-MultiDictHelper.prototype.queryDictionaryObject = function(name,pdfReader) {
-    const dict = _.find(this.dicts,(d)=> {
-        return d.exists(name);
-    });
-
-    if(!!dict) {
-        return pdfReader.queryDictionaryObject(dict,name);
+// eslint-disable-next-line no-use-before-define
+class MultiDictHelper {
+    constructor(dicts) {
+        this.dicts = dicts;
     }
-    else 
+
+    exists(name) {
+        return _.some(this.dicts, (dict) => {
+            return dict.exists(name);
+        });
+    }
+
+    queryDictionaryObject(name, pdfReader) {
+        const dict = _.find(this.dicts, (d) => {
+            return d.exists(name);
+        });
+
+        if(dict) {
+            return pdfReader.queryDictionaryObject(dict, name);
+        }
+        
         return null;
+    }
 }
 
 module.exports = MultiDictHelper
